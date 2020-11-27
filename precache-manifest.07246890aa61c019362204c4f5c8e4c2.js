d3f949de95f92a9254308c46d2171781ea84b436
Lkhagvanyam <best0728.b@gmail.com>
2020-11-28 01:31:36 +0800

    Updates

---
+++ precache-manifest.07246890aa61c019362204c4f5c8e4c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2519b1b67ba4860190e4dd0a7f1a5a",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "f17656621d9bec489caa",
+    "url": "/burger/static/css/main.69647f5f.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17656621d9bec489caa",
+    "url": "/burger/static/js/main.cb6e72ce.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
